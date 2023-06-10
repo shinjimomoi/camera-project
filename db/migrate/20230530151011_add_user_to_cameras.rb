03d9862c0efd98c5167682bfe442745bdf3147ed
Shinji Momoi <shinjimomoi@gmail.com>
2023-06-11 00:30:51 +0900

    changed links on the cameras show page

--- db/migrate/20230530151011_add_user_to_cameras.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToCameras < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :cameras, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -54,12 +54,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_144756) do
     t.string "brand"
     t.string "model"
     t.integer "year"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
     t.string "condition"
-    t.integer "user_id", null: false
-    t.string "photo"
     t.index ["user_id"], name: "index_cameras_on_user_id"
   end
 
