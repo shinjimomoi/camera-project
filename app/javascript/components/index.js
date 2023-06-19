db0c1f95cbba75fb998ce7e1dc6868c9e779e7f3
Ricardo Momoi <shinjimomoi@gmail.com>
2023-06-20 00:46:44 +0900

    styled a little of filter

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Your JavaScript code here
+  let filterTitles = document.querySelectorAll(".filters p");
 
+  filterTitles.forEach((title) => {
+    title.addEventListener('click', () => {
+      let filterName = title.getAttribute('data-filter');
+      let filter = document.querySelector(`.filter.${filterName}`);
+
+      if (filter.classList.contains('hidden')) {
+        filter.classList.remove('hidden');
+      } else {
+        filter.classList.add('hidden');
+      }
+    });
+  });
 });
