adc363b2a041d636d880a897c906a401bdfe18b1
Ricardo Momoi <shinjimomoi@gmail.com>
2023-06-20 01:01:40 +0900

    added more style to filters

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   let filterTitles = document.querySelectorAll(".filters p");
+  let filters = document.querySelectorAll(".filter");
 
   filterTitles.forEach((title) => {
     title.addEventListener('click', () => {
       let filterName = title.getAttribute('data-filter');
       let filter = document.querySelector(`.filter.${filterName}`);
 
-      if (filter.classList.contains('hidden')) {
-        filter.classList.remove('hidden');
-      } else {
-        filter.classList.add('hidden');
-      }
+      filters.forEach((filterElement) => {
+        if (filterElement !== filter) {
+          filterElement.classList.add('hidden');
+        }
+      });
+
+      filter.classList.toggle('hidden');
     });
   });
 });
