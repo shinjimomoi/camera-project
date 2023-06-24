24af26f1db13a8735b622f28f83a182e1e6bc9c2
Shinji Momoi <shinjimomoi@gmail.com>
2023-06-24 17:46:47 +0900

    adjusted responsiveness for reviews

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def render_stars(rating)
+        filled_stars = '★' * rating
+        empty_stars = '☆' * (5 - rating)
+        filled_stars + empty_stars
+    end
 end
